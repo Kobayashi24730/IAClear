7c39588407180ef7adac95e1b3c3db78249c7408
Kobayashi24730 <ajenteduplo000@gmail.com>
2025-11-30 11:53:28 -0300

    Change BASE_URL to production endpoint

--- frontend/src/assets/api.js
+++ frontend/src/assets/api.js
@@ -1,6 +1,6 @@
 // src/assets/api.js
 
-const BASE_URL = "http://10.0.0.113:8000"; // <<< COLOQUE SEU IP LOCAL AQUI
+const BASE_URL = "https://iaclear-1-backend.onrender.com"; // <<< COLOQUE SEU IP LOCAL AQUI
 
 export async function perguntarIA(rota, pergunta, projeto) {
   const req = await fetch(`${BASE_URL}${rota}`, {
