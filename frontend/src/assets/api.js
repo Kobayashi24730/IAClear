aede8b4d070fb5ad000ff873fe9cdb5f29bc52c1
Kobayashi24730 <ajenteduplo000@gmail.com>
2025-11-30 22:26:04 -0300

    Remove local IP comment from api.js

--- frontend/src/assets/api.js
+++ frontend/src/assets/api.js
@@ -1,6 +1,6 @@
 // src/assets/api.js
 
-const BASE_URL = "https://iaclear-1-backend.onrender.com"; // <<< COLOQUE SEU IP LOCAL AQUI
+const BASE_URL = "https://iaclear-1-backend.onrender.com";
 
 export async function perguntarIA(rota, pergunta, projeto) {
   const req = await fetch(`${BASE_URL}${rota}`, {
